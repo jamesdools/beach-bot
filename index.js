2145a805a2f155dbe227326916662ec3ae03ccaf
James Dooley <james.dooley@bbc.co.uk>
2021-03-15 18:41:48 +0000

    Task: Refactor

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ const queue = new Map();
 const userPreferences = new Map();
 
 const DEFAULT_SONG = {
-  title: 'My Rage Knows No Bounds!',
-  url: 'https://www.youtube.com/watch?v=TNLPGCCUfnY'
+  title: 'I AM UNTETHERED AND MY RAGE KNOWS NO BOUNDS',
+  url: 'https://www.youtube.com/watch?v=LL4JE5N_S5o',
 };
 
 const parseArgs = (content) => {
@@ -41,33 +41,19 @@ const parseArgs = (content) => {
   return { args, command };
 };
 
-bot.on('message', async (message) => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  console.log('guild id: ' + message.guild.id);
-  console.log('\n');
-
-  if (message.content.startsWith(prefix + 'beach')) {
-    console.log(message.channel);
-    message.channel.send('shirt!');
-  } else if (message.content.startsWith(prefix + 'entrance')) {
-    checkVoice(message);
-    await entranceMusic(message);
-  }
-});
-
 const checkVoice = (message) => {
   const voiceChannel = message.member.voice.channel;
+
   if (!voiceChannel) {
     return message.channel.send('You need to be in a voice channel to play music!');
   }
 
   const permissions = voiceChannel.permissionsFor(message.client.user);
+
   if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-    return message.channel.send('BeachBot doesn\'t have permission to join and speak in this channel!');
+    return message.channel.send("BeachBot doesn't have permission to join and speak in this channel!");
   }
-}
+};
 
 const entranceMusic = async (message) => {
   const { args, command } = parseArgs(message.content);
@@ -89,132 +75,89 @@ const entranceMusic = async (message) => {
   userPreferences.set(user, song);
 
   return message.channel.send(`${name} entrance music set: {${song.title}`);
-  // const serverQueue = queue.get(message.guild.id);
-
-  // if (!serverQueue) {
-  //   const queueContruct = {
-  //     textChannel: message.channel,
-  //     voiceChannel: voiceChannel,
-  //     connection: null,
-  //     songs: [],
-  //     volume: 5,
-  //     playing: true,
-  //   };
-
-  //   queue.set(message.guild.id, queueContruct);
-  //   queueContruct.songs.push(song);
-
-  //   try {
-  //     const connection = await voiceChannel.join();
-
-  //     queueContruct.connection = connection;
-
-  //     play(message.guild, queueContruct.songs[0]);
-  //   } catch (err) {
-  //     console.log(err);
-  //     queue.delete(message.guild.id);
-
-  //     return message.channel.send(err);
-  //   }
-  // } else {
-  //   serverQueue.songs.push(song);
-
-  //   return message.channel.send(`${song.title} added to the queue!`);
-  // }
 };
 
-function play(guild, song, connection) {
+const play = (guild, song) => {
   const serverQueue = queue.get(guild.id);
+
   if (!song) {
     serverQueue.voiceChannel.leave();
     queue.delete(guild.id);
     return;
   }
 
-  // const dispatcher = serverQueue.connection
-  const dispatcher = connection
+  const dispatcher = serverQueue.connection
     .play(ytdl(song.url))
-    .on("finish", () => {
+    .on('finish', () => {
       serverQueue.songs.shift();
       play(guild, serverQueue.songs[0]);
     })
-    .on("error", error => console.error(error));
+    .on('error', (error) => console.error(error));
 
   dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+
+  if (serverQueue.textChannel) {
+    serverQueue.textChannel.send(`Now playing: **${song.title}**`);
+  }
 }
 
+bot.on('message', async (message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
-// bot.on('message', async (message) => {
-bot.on('voiceStateUpdate', async (oldState, newState) => {
-  console.log('voiceStateUpdate');
-  
-  const oldChannel = oldState.channelID;
-  const newChannel = newState.channelID;
-  console.log('voice channel:' + newChannel);
+  console.log('guild id: ' + message.guild.id);
   console.log('\n');
 
-  if (!oldChannel && newChannel) {
-    const user = newState.id;
-    console.log(`New user joined channel: ${newState.guild.name}`);
-
-    const userSong = userPreferences.get(user) || DEFAULT_SONG;
-
-    try {
-      const connection = await newState.channel.join();
-
-      play(newState.guild, userSong, connection);
-    } catch (err) {
-      console.log(err);
-    }
+  if (message.content.startsWith(prefix + 'beach')) {
+    console.log(message.channel);
+    message.channel.send('shirt!');
+  } else if (message.content.startsWith(prefix + 'entrance')) {
+    checkVoice(message);
+    await entranceMusic(message);
   }
 });
 
-  //   // getSongId for User
-  //   const songUrl = 'https://www.youtube.com/watch?v=TNLPGCCUfnY';
+bot.on('voiceStateUpdate', async (oldState, newState) => {
+  console.log('voiceStateUpdate');
 
-  //   const songInfo = await ytdl.getInfo(songUrl);
-  //   const song = {
-  //     title: songInfo.videoDetails.title,
-  //     url: songInfo.videoDetails.video_url,
-  //   };
+  const oldChannel = oldState.channelID;
+  const newChannel = newState.channelID;
 
-  // console.log(`Song: ${song.title}`);
+  if (!oldChannel && newChannel) {
+    console.log(`New user joined channel: ${newState.guild.name}`);
 
-  // const serverQueue = queue.get(newUserState.guild.id);
-  // const messageChannel = '';
-  
-  // if (!serverQueue) {
-  //   const queueContruct = {
-  //     textChannel: messageChannel,
-  //     voiceChannel: newChannel,
-  //     connection: null,
-  //     songs: [],
-  //     volume: 5,
-  //     playing: true,
-  //   };
+    const user = newState.id;
+    const song = userPreferences.get(user) || DEFAULT_SONG;
 
-  //   queue.set(newUserState.guild.id, queueContruct);
-  //   queueContruct.songs.push(song);
+    const serverQueue = queue.get(newState.guild.id);
 
-  //   try {
-  //     const connection = await newUserState.join();
+    if (!serverQueue) {
+      const queueContruct = {
+        textChannel: '',
+        voiceChannel: newState.channel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true,
+      };
 
-  //     queueContruct.connection = connection;
+      queue.set(newState.guild.id, queueContruct);
+      queueContruct.songs.push(song);
 
-  //     play(newUserState.guild, queueContruct.songs[0]);
-  //   } catch (err) {
-  //     console.log(err);
-  //     queue.delete(newUserState.guild.id);
+      try {
+        const connection = await newState.channel.join();
+        
+        queueContruct.connection = connection;
 
-  //     // return message.channel.send(err);
-  //     return;
-  //   }
-  // } else {
-  //   serverQueue.songs.push(song);
+        play(newState.guild, song, connection);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+        serverQueue.songs.push(song);
 
-  //   // return message.channel.send(`${song.title} added to the queue!`);
-  //   return;
-  // }
-    // } 
+        console.log(`${song.title} queued up!`);
+    }
+  }
+});
 
--- test/folders/voice.md
+++ test/folders/voice.md
@@ -1,89 +1,89 @@
 GuildMember {
-  guild: <ref *1> Guild {
-    members: GuildMemberManager {
-      cacheType: [class Collection extends Collection],
-      cache: [Collection [Map]],
-      guild: [Circular *1]
-    },
-    channels: GuildChannelManager {
-      cacheType: [class Collection extends Collection],
-      cache: [Collection [Map]],
-      guild: [Circular *1]
-    },
-    roles: RoleManager {
-      cacheType: [class Collection extends Collection],
-      cache: [Collection [Map]],
-      guild: [Circular *1]
-    },
-    presences: PresenceManager {
-      cacheType: [class Collection extends Collection],
-      cache: Collection(0) [Map] {}
-    },
-    voiceStates: VoiceStateManager {
-      cacheType: [class Collection extends Collection],
-      cache: [Collection [Map]],
-      guild: [Circular *1]
-    },
-    deleted: false,
-    available: true,
-    id: '821037320635547649',
-    shardID: 0,
-    name: 'JD',
-    icon: null,
-    splash: null,
-    discoverySplash: null,
-    region: 'europe',
-    memberCount: 2,
-    large: false,
-    features: [],
-    applicationID: null,
-    afkTimeout: 300,
-    afkChannelID: null,
-    systemChannelID: '821037320635547653',
-    embedEnabled: undefined,
-    premiumTier: 0,
-    premiumSubscriptionCount: 0,
-    verificationLevel: 'NONE',
-    explicitContentFilter: 'DISABLED',
-    mfaLevel: 0,
-    joinedTimestamp: 1615820979725,
-    defaultMessageNotifications: 'ALL',
-    systemChannelFlags: SystemChannelFlags { bitfield: 0 },
-    maximumMembers: 100000,
-    maximumPresences: null,
-    approximateMemberCount: null,
-    approximatePresenceCount: null,
-    vanityURLCode: null,
-    vanityURLUses: null,
-    description: null,
-    banner: null,
-    rulesChannelID: null,
-    publicUpdatesChannelID: null,
-    preferredLocale: 'en-US',
-    ownerID: '365579546256343060',
-    emojis: GuildEmojiManager {
-      cacheType: [class Collection extends Collection],
-      cache: Collection(0) [Map] {},
-      guild: [Circular *1]
-    }
-  },
-  joinedTimestamp: 1615820951380,
-  lastMessageID: '821075566504706069',
-  lastMessageChannelID: '821037320635547653',
-  premiumSinceTimestamp: null,
-  deleted: false,
-  nickname: null,
-  _roles: [],
-  user: User {
-    id: '365579546256343060',
-    system: null,
-    locale: null,
-    flags: UserFlags { bitfield: 0 },
-    username: 'JD',
-    bot: false,
-    discriminator: '5768',
-    avatar: '87fcf9412a596f88a48177f00bcd8584',
-    lastMessageID: '821075566504706069',
-    lastMessageChannelID: '821037320635547653'
-  }
+guild: <ref *1> Guild {
+members: GuildMemberManager {
+cacheType: [class Collection extends Collection],
+cache: [Collection [Map]],
+guild: [Circular *1]
+},
+channels: GuildChannelManager {
+cacheType: [class Collection extends Collection],
+cache: [Collection [Map]],
+guild: [Circular *1]
+},
+roles: RoleManager {
+cacheType: [class Collection extends Collection],
+cache: [Collection [Map]],
+guild: [Circular *1]
+},
+presences: PresenceManager {
+cacheType: [class Collection extends Collection],
+cache: Collection(0) [Map] {}
+},
+voiceStates: VoiceStateManager {
+cacheType: [class Collection extends Collection],
+cache: [Collection [Map]],
+guild: [Circular *1]
+},
+deleted: false,
+available: true,
+id: '821037320635547649',
+shardID: 0,
+name: 'JD',
+icon: null,
+splash: null,
+discoverySplash: null,
+region: 'europe',
+memberCount: 2,
+large: false,
+features: [],
+applicationID: null,
+afkTimeout: 300,
+afkChannelID: null,
+systemChannelID: '821037320635547653',
+embedEnabled: undefined,
+premiumTier: 0,
+premiumSubscriptionCount: 0,
+verificationLevel: 'NONE',
+explicitContentFilter: 'DISABLED',
+mfaLevel: 0,
+joinedTimestamp: 1615820979725,
+defaultMessageNotifications: 'ALL',
+systemChannelFlags: SystemChannelFlags { bitfield: 0 },
+maximumMembers: 100000,
+maximumPresences: null,
+approximateMemberCount: null,
+approximatePresenceCount: null,
+vanityURLCode: null,
+vanityURLUses: null,
+description: null,
+banner: null,
+rulesChannelID: null,
+publicUpdatesChannelID: null,
+preferredLocale: 'en-US',
+ownerID: '365579546256343060',
+emojis: GuildEmojiManager {
+cacheType: [class Collection extends Collection],
+cache: Collection(0) [Map] {},
+guild: [Circular *1]
+}
+},
+joinedTimestamp: 1615820951380,
+lastMessageID: '821075566504706069',
+lastMessageChannelID: '821037320635547653',
+premiumSinceTimestamp: null,
+deleted: false,
+nickname: null,
+\_roles: [],
+user: User {
+id: '365579546256343060',
+system: null,
+locale: null,
+flags: UserFlags { bitfield: 0 },
+username: 'JD',
+bot: false,
+discriminator: '5768',
+avatar: '87fcf9412a596f88a48177f00bcd8584',
+lastMessageID: '821075566504706069',
+lastMessageChannelID: '821037320635547653'
+}
 }
